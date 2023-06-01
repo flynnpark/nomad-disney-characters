dacec2aeb1cee15bb9ba07360aaed507d5bf782b
Flynn <wphestiraid@gmail.com>
2023-06-01 20:31:11 +0900

    :wrench: ESLint 규칙 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
   },
-}
+};
