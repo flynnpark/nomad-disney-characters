a8c6a79ca097bac387c1e7c56f0e89b1c9e0d102
Flynn <wphestiraid@gmail.com>
2023-06-03 20:54:28 +0900

    :construction: 디렉토리 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from './components/organisms/Header';
+import Header from './components/Header';
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router.tsx';
-
-const queryClient = new QueryClient();
+import { queryClient } from './utils/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
