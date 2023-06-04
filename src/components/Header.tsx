c85f8113a494dd3ff3d46e17809ba8c9804b138a
Flynn <wphestiraid@gmail.com>
2023-06-04 20:25:31 +0900

    :sparkles: 헤더에 깃허브 링크로 가는 메뉴 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,31 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { MAX_WIDTH } from '../utils/constants';
+import { GithubIcon } from './icons';
 
 const Nav = styled.nav`
   display: block;
 `;
 
 const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   max-width: ${MAX_WIDTH}px;
   padding: 0 16px;
   margin: auto;
+  justify-content: space-between;
 `;
 
 const LogoContainer = styled.div`
   font-size: 24px;
   font-weight: 900;
   padding: 24px 0;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  justify-items: center;
+  align-items: center;
 `;
 
 function Header() {
@@ -26,6 +36,11 @@ function Header() {
         <LogoContainer>
           <Link to="/">Disney Characters</Link>
         </LogoContainer>
+        <MenuContainer>
+          <a href="https://github.com/flynnpark/nomad-disney-characters" target="_blank">
+            <GithubIcon size={32} />
+          </a>
+        </MenuContainer>
       </NavContainer>
     </Nav>
   );
