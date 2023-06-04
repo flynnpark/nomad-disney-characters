4eb67dda84635d87bcbd6a48053cd5f713f74614
Flynn <wphestiraid@gmail.com>
2023-06-04 19:24:27 +0900

    :lipstick: 간단한 스타일링
    - 헤더 및 컨텐츠 영역을 위한 간단한 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Header from './components/Header';
+import { MAX_WIDTH } from './utils/constants';
+
+const ContentsContainer = styled.div`
+  max-width: ${MAX_WIDTH}px;
+  margin: auto;
+`;
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <ContentsContainer>
+        <Outlet />
+      </ContentsContainer>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { MAX_WIDTH } from '../utils/constants';
+
+const Nav = styled.nav`
+  display: block;
+`;
+
+const NavContainer = styled.div`
+  max-width: ${MAX_WIDTH}px;
+  padding: 0 16px;
+  margin: auto;
+`;
+
+const LogoContainer = styled.div`
+  font-size: 24px;
+  font-weight: 900;
+  padding: 24px 0;
+`;
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Nav>
+      <NavContainer>
+        <LogoContainer>
+          <Link to="/">Disney Characters</Link>
+        </LogoContainer>
+      </NavContainer>
+    </Nav>
+  );
 }
 
 export default Header;
