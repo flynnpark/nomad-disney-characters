0aca424c8d55cd8d800572c01667189ca71d746d
Flynn <wphestiraid@gmail.com>
2023-06-04 19:27:12 +0900

    :art: 메인 화면 캐릭터 목록 수정

--- src/components/CharacterIcon.tsx
+++ src/components/CharacterIcon.tsx
@@ -1,14 +1,42 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import notFoundImage from '../assets/not_found.png';
+
+const Container = styled.div`
+  display: flex;
+  padding: auto;
+  text-align: center;
+  justify-content: center;
+  margin: 24px 0;
+`;
+
+const Avatar = styled.img`
+  vertical-align: middle;
+  object-fit: cover;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+
+const CharacterName = styled.p`
+  margin-top: 16px;
+`;
 
 interface CharacterIconProps {
   character: Character;
 }
 
 function CharacterIcon({ character }: CharacterIconProps) {
+  const { imageUrl, name } = character;
+  console.log(name, imageUrl);
   return (
-    <Link to={`characters/${character.id}`}>
-      <div>{character.name}</div>
-    </Link>
+    <Container>
+      <Link to={`characters/${character.id}`}>
+        <Avatar src={imageUrl ?? notFoundImage} alt={name} />
+        <CharacterName>{name}</CharacterName>
+      </Link>
+    </Container>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
+import styled from 'styled-components';
 
 import CharacterIcon from '../../components/CharacterIcon';
 import { queryKey } from './loader';
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
 function Home() {
   const { data: characters } = useQuery<GetCharactersResponse>({ queryKey });
   return (
-    <>
+    <Container>
       {characters?.map((character) => (
         <CharacterIcon key={character.id} character={character} />
       ))}
-    </>
+    </Container>
   );
 }
 
